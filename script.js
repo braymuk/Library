5d4f42c6949ed4ebd1707753bb015e14cba3ec4b
braymuk <braydeng9@gmail.com>
2021-07-02 23:55:55 -0700

    Basic layout

---
+++ script.js
@@ -0,0 +1,13 @@
+let myLibrary = [];
+
+function Book(title, author, pages, date, isRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.date = date;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {
+
+}
