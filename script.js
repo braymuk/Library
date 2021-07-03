e953d954ddaf65eb6e6a505de91b78737e011d49
braymuk <braydeng9@gmail.com>
2021-07-03 00:14:35 -0700

    Added book.rating

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 let myLibrary = [];
 
-function Book(title, author, pages, date, isRead) {
+function Book(title, author, pages, date, isRead, rating) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.date = date;
     this.isRead = isRead;
+    this.rating = rating;
 }
 
 
@@ -13,7 +14,10 @@ function Book(title, author, pages, date, isRead) {
 //  but should this also handle the DOM?
 const libraryContainer = document.querySelector('#libraryContainer');
 
+
+//This could probably be written much better
 function addBookToLibrary(book) {
+    //Pushes book data to library array
     myLibrary.push(book);
     
    let bookElem = document.createElement('div');
@@ -35,14 +39,19 @@ function addBookToLibrary(book) {
    bookElem.appendChild(bookPages);
 
    let bookDate = document.createElement('span');
-   bookDate.textContent = "Published" + book.date;
+   bookDate.textContent = "Published: " + book.date;
    bookTitle.classList.add('bookDate');
    bookElem.appendChild(bookDate);
+
+   let bookRating = document.createElement('span');
+   bookRating.textContent = "Rating: " + book.rating;
+   bookTitle.classList.add('bookRating');
+   bookElem.appendChild(bookRating);
 
     libraryContainer.appendChild(bookElem);
 }
 
-const book1 = new Book("Star Wars", "George Lucas", "123", "1996", false);
+const book1 = new Book("Star Wars", "George Lucas", "123", "1996", false, 3);
 
 
 addBookToLibrary(book1);
